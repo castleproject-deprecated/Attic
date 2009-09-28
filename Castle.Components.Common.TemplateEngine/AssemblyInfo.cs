6d03276e9ab3bcb906c276d4b74fb111f26ac7f3
Jonathon Rossi <jono@jonorossi.com>
2009-09-28 12:45:39 +0000

    Pulling the TemplateEngine project into its own trunk.
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/Components/TemplateEngine/trunk@6013 73e77b4c-caa6-f847-a29a-24ab75ae54b6

---
+++ Castle.Components.Common.TemplateEngine/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: AssemblyTitle("Castle.Components.Common.TemplateEngine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Castle Project")]
+[assembly: AssemblyProduct("Castle.Components.Common.TemplateEngine")]
+[assembly: AssemblyCopyright("Castle Project, original author or authors")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyDelaySign(false)]
